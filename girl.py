906370f37ae3c750d4cffa9d702a2ae6f4e64400
Hoang Anh <doviethoanganh07@gmail.com>
2025-05-22 23:36:39 +0700

    Update girl.py

--- girl.py
+++ girl.py
@@ -8,22 +8,26 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['girl'])
 def handle_girl(message):
     try:
+        # Lấy video URL
         api_url = "https://api-l8y6.onrender.com/api/random-video"
         response = requests.get(api_url, timeout=10).json()
         video_url = response['video_url']
         
-        bot.send_video(
-            chat_id=message.chat.id,
-            video=video_url,
-            reply_to_message_id=message.message_id,
-            timeout=20
-        )
+        try:
+            # Gửi video
+            bot.send_video(
+                chat_id=message.chat.id,
+                video=video_url,
+                reply_to_message_id=message.message_id,
+                timeout=20
+            )
+        except:
+            # Lỗi gửi video - URL bị lỗi
+            bot.reply_to(message, f"Link lỗi: {video_url}")
             
     except:
-        if 'video_url' in locals():
-            bot.reply_to(message, f"Link lỗi: {video_url}")
-        else:
-            bot.reply_to(message, "Lỗi API!")
+        # Lỗi gọi API
+        bot.reply_to(message, "Lỗi API!")
 
 # Khởi chạy bot
 print("Bot random video gái đang chạy...")
