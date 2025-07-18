41e5206f87f0cecabfb997d4c80acf94b35d42dd
Hoang Anh <doviethoanganh07@gmail.com>
2025-07-18 18:31:02 +0700

    Update img.py

--- img.py
+++ img.py
@@ -3,21 +3,24 @@ import time
 import telebot
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHANNEL_ID = "-1002795920037"  # hoặc -100xxxxxxxxxx
+CHANNEL_ID = "-1002795920037888888888888888888"  # ID kênh
 
 bot = telebot.TeleBot(TOKEN)
 
 def send_images():
-    with open("cosplay_links.txt", "r") as f:
-        urls = [line.strip() for line in f if line.strip()]
+	with open("url.txt", "r") as f:
+		urls = [line.strip() for line in f if line.strip()]
 
-    for url in urls:
-        try:
-            bot.send_photo(CHANNEL_ID, url)
-        except:
-            pass
-        time.sleep(5)
+	for url in urls:
+		try:
+			if url.lower().endswith(".gif"):
+				bot.send_animation(CHANNEL_ID, url)
+			else:
+				bot.send_photo(CHANNEL_ID, url)
+		except Exception as e:
+			error_msg = f"⚠️ Không gửi được:\n{url}\nLý do: {e}"
+			bot.send_message(CHANNEL_ID, error_msg)
+		time.sleep(5)
 
 if __name__ == "__main__":
-    # time.sleep(300)
-    send_images()
+	send_images()
